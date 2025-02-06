5366b0302d7e9b38f4aecaa99f7b0a7dd6d56520
RickFerry <rick_ferry1@hotmail.com>
2025-02-06 16:25:27 -0300

    JPA and H2 configurations, and seeding

---
+++ src/main/java/com/vendify/model/Sale.java
@@ -0,0 +1,29 @@
+package com.vendify.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_sales")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private BigDecimal amount;
+    private LocalDate date;
+}
+
